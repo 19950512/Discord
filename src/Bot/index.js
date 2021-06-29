af644820720cf811d6b5360f2555e9ce1ee22000
Maydana <mattmaydana@gmail.com>
2021-06-29 01:36:35 -0300

    29/06/2021 - Primeiras pastas e .env

---
+++ src/Bot/index.js
@@ -0,0 +1,4 @@
+const { BOT_NOME } = require('../config')
+
+console.log('######### BOT #########\n')
+console.log(`Bot Nome: ${BOT_NOME}`)
---
+++ src/Webhooks/index.js
@@ -0,0 +1,4 @@
+const { BOT_NOME } = require('../config')
+
+console.log('######### Webhooks #########\n')
+console.log(`Bot Nome: ${BOT_NOME}`)
