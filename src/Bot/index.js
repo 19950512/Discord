3d63e24cd97569a78167ef16be4645d35bcda84b
Matheus <developer.web42@gmail.com>
2022-01-11 14:44:09 -0300

    11/01/2022 - Check Version Nodejs
    -

--- src/Bot/index.js
+++ src/Bot/index.js
@@ -1,3 +1,15 @@
+let versionAtual = process.version.replace('v', '').split('.')[0]
+
+const versaoNecessaria = '14.17.1 >='
+
+if(versionAtual < versaoNecessaria.split('.')[0]){
+    console.log('Você precisa usar uma versão mais atual do NODE.')
+    console.log(`Versão necessária: ${versaoNecessaria}`)
+    console.log(`Sua versão: ${process.version}`)
+    process.exit(1)
+}
+
+
 const { BOT_NOME, BOT_PREFIXO, API_TOKEN } = require('../config')
 
 console.log('######### BOT #########\n')
